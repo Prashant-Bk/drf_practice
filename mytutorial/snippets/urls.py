3a5bb97dc746595a6269b4574570152865bdbdb0
Prashant bk <prashantbk314@gmail.com>
2024-12-11 10:03:21 +0545

    added root endpoint and api that gives html highlights

--- mytutorial/snippets/urls.py
+++ mytutorial/snippets/urls.py
@@ -3,14 +3,20 @@ from .views import (SnippetList,
                     SnippetDetail,
                     UserList,
                     UserRetrieve,
-                    make_request)
+                    SnippetHighlight,
+                    api_root,
+                    make_request ,
+                    get_users_highlights)
 
 urlpatterns = [
+    path('',api_root , name = "api_root") , 
     path('snippets/', SnippetList.as_view() , name = "snippet_list"),
     path('snippets/<int:pk>/', SnippetDetail.as_view() , name = "snippet_detail"),
+    path('snippets/<int:id>/highlight/', SnippetHighlight.as_view() , name = "snippet_highlighted"),
     path('users/',UserList.as_view() , name = "user_list" ),
     path('users/<int:pk>/',UserRetrieve.as_view() , name = "user_list" ),
-    path('make_request/' , make_request)
+    path('snippets/<str:username>/highlights/',get_users_highlights , name = "user_highlights" ),
+    path('request_info/' , make_request)
 ]
 
 
--- mytutorial/snippets/views.py
+++ mytutorial/snippets/views.py
@@ -1,12 +1,32 @@
 from snippets.models import Snippet 
 from django.contrib.auth.models import User
-from snippets.serializers import SnippetSerializer , UserSerializer
+from snippets.serializers import SnippetSerializer
+from snippets.serializers import  UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view , renderer_classes
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+from django.http import HttpRequest 
 
 
 
+
+@api_view(http_method_names=["GET"])
+def api_root(request , format = None):
+    username = request.user.username
+    return Response(
+        data = {
+            "snippet":reverse("snippet_list", request=request, format=format),
+            "user":reverse("user_list", request=request, format=format),
+            "request info":reverse(make_request, request=request, format=format),
+            "my highlights":reverse(get_users_highlights ,args=[username], request=request, format=format)
+        })
+    
+      
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     #if user is authenticated it can read and write else read only
@@ -29,10 +49,34 @@ class UserList(generics.ListAPIView): #adding read only views
     queryset = User.objects.all()
     serializer_class = UserSerializer
  
+ 
 class UserRetrieve(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     
+    
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+    lookup_field = "id"
+    
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object() #use pk here
+        print(snippet.highlighted)
+        return Response([snippet.highlighted])
+        
+       
+    
+#To get all the highlighted snippets of a user
+@api_view(http_method_names=["GET"])
+@renderer_classes([renderers.StaticHTMLRenderer])
+#applying html renderer to @api based view
+def get_users_highlights(request , username):
+    user = User.objects.get(username = username)
+    user_snippets = user.snippets.all()
+    user_snippets_highlighted = [snippet.highlighted for snippet in user_snippets]
+    return Response(user_snippets_highlighted)
+
 
 # for checking current request and user
 from rest_framework.decorators import api_view
@@ -47,7 +91,6 @@ def make_request(http_request):
     print("\ntype(request): ", type(http_request))
     print("\nuser: ", http_request.user )
     print("\ntype(user): ", type(http_request.user) )
-    
     data = {
         "request":       str(http_request) , 
         "type(request)": str( type(http_request)) , 
